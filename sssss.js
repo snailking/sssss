ec14b841f4a8c81e8246f14a02b5d6461d544bd6
SnailKing <39387494+snailking@users.noreply.github.com>
2019-03-06 22:29:06 +0100

    Add files via upload

--- sssss.js
+++ sssss.js
@@ -167,7 +167,7 @@ function fastUpdate(){
 
 //Format event name/address into a clickable name that sets address in the rename field
 function formatName(_name, _adr){
-	var _stringLink = '<a onClick="sendFieldOther(' + _adr + ')">' + _name + '</a>';
+	var _stringLink = '<a onClick="sendFieldOther('+ _adr + ')">' + _name + '</a>';
 	return _stringLink;
 }
 
@@ -956,7 +956,7 @@ function runLog(){
 							startBlock = result[i].blockNumber; //store the last blocknumber to start next loop
 							dateLog(result[i].blockNumber);
 							if(result[i].event == "JoinedGame"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + formatName(result[i].args.sender, result[i].args.player) + " enters the Sloth Arena.";								
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + formatName(result[i].args.player, result[i].args.sender) + " enters the Sloth Arena.";								
 							} else if(result[i].event == "Hatched"){
 								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + result[i].args.player + " hatched " + result[i].args.eggUsed + " eggs (+" + result[i].args.eggBonus + " bonus) into " + result[i].args.newSnail + " snails. Their " + idToName(web3.toDecimal(result[i].args.nest)) + " nest has " + result[i].args.nestCount + " snails.";		
 							} else if(result[i].event == "ChangedMessage"){
