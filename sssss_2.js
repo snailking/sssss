2c3ace872287a09d4d98182660a89fa195b8450f
SnailKing <39387494+snailking@users.noreply.github.com>
2019-03-06 22:14:59 +0100

    Add files via upload

--- sssss_2.js
+++ sssss_2.js
@@ -947,19 +947,19 @@ function runLog(){
 							if(result[i].event == "JoinedGame"){
 								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + result[i].args.player + " enters the Sloth Arena.";								
 							} else if(result[i].event == "Hatched"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + toAscii(result[i].args.player) + " hatched " + result[i].args.eggUsed + " eggs (+" + result[i].args.eggBonus + " bonus) into " + result[i].args.newSnail + " snails. Their " + idToName(web3.toDecimal(result[i].args.nest)) + " nest has " + result[i].args.nestCount + " snails.";		
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + result[i].args.player + " hatched " + result[i].args.eggUsed + " eggs (+" + result[i].args.eggBonus + " bonus) into " + result[i].args.newSnail + " snails. Their " + idToName(web3.toDecimal(result[i].args.nest)) + " nest has " + result[i].args.nestCount + " snails.";		
 							} else if(result[i].event == "ChangedMessage"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + toAscii(result[i].args.player) + " changed the message to: " + toAscii(result[i].args.message);
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + result[i].args.player + " changed the message to: " + result[i].args.message;
 							} else if(result[i].event == "NamedPlayer"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] From now on, " + toAscii(result[i].args.player) + " will be known as " + toAscii(result[i].args.name);			
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] From now on, " + result[i].args.player + " will be known as " + result[i].args.name;			
 							} else if(result[i].event == "RenamedOther"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] Say goodbye to " + toAscii(result[i].args.previousName) + "! " + toAscii(result[i].args.player) + " decided on their new name: " + toAscii(result[i].args.name);
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] Say goodbye to " + result[i].args.previousName + "! " + result[i].args.player + " decided on their new name: " + result[i].args.name;
 							} else if(result[i].event == "VenturedLair"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + toAscii(result[i].args.player) + " sends " + result[i].args.fighterCount + " " + idToName(web3.toDecimal(result[i].args.nest)) + " snails to fight the Sloth. We estimate their odds of success at " + result[i].args.odd + "%";
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] " + result[i].args.player + " sends " + result[i].args.fighterCount + " " + idToName(web3.toDecimal(result[i].args.nest)) + " snails to fight the Sloth. We estimate their odds of success at " + result[i].args.odd + "%";
 							} else if(result[i].event == "WonFight"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] VICTORY! The sloth rolls a " + result[i].args.result + " against " + toAscii(result[i].args.player) + "'s " + result[i].args.odd + ", and rolls over. " + idToName(web3.toDecimal(result[i].args.nest)) + " snails take home " + result[i].args.reward + " of their friends.";
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] VICTORY! The sloth rolls a " + result[i].args.result + " against " + result[i].args.player + "'s " + result[i].args.odd + ", and rolls over. " + idToName(web3.toDecimal(result[i].args.nest)) + " snails take home " + result[i].args.reward + " of their friends.";
 							} else if(result[i].event == "LostFight"){
-								eventlogdoc.innerHTML += "<br>[" + datetext + "] DEFEAT! The sloth munches on " + result[i].args.fighterCount + " snails tonight, with a "  + result[i].args.result + " against " + toAscii(result[i].args.player) + "'s " + result[i].args.odd;
+								eventlogdoc.innerHTML += "<br>[" + datetext + "] DEFEAT! The sloth munches on " + result[i].args.fighterCount + " snails tonight, with a "  + result[i].args.result + " against " + result[i].args.player + "'s " + result[i].args.odd;
 							}
 							logboxscroll.scrollTop = logboxscroll.scrollHeight;
 						}
